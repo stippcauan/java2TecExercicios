175f3483053009b5659c3b5e4f041b5c88160aba
Cauan stipp <cauan.stipp@escola.pr.gov.br>
2026-06-24 16:41:43 +0000

    finalizado

--- Exercicio6.java
+++ Exercicio6.java
@@ -1,14 +1,28 @@
 import java.util.Scanner;
+
 public class Exercicio6 {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+      
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o valor da compra");
-        double valorCompra = scanner.nextDouble();
 
-        if(valorCompra >= 200){
-            Double valorDescontado = valorCompra *0.8;
-            System.out.println("O valor da sua compra ficou R$ " + valorDescontado);
-        } else if(valorCompra)
+        System.out.print("Digite o valor total da compra: ");
+        double valorTotal = scanner.nextDouble();
+
+        int porcentagemDesconto = 0;
+
+        if (valorTotal > 200) {
+            porcentagemDesconto = 20;
+        } else if (valorTotal > 100) {
+            porcentagemDesconto = 10;
+        } else {
+            porcentagemDesconto = 0;
+        }
+
+
+        double valorDesconto = valorTotal * (porcentagemDesconto / 100.0);
+        double valorFinal = valorTotal - valorDesconto;
+
+        System.out.println("Desconto aplicado: " + porcentagemDesconto + "%");
+    
     }
-}
+}        
